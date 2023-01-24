fead0758e1244233b989bbc2475d8a1a291c8b81
suryakaushik46 <62637092+suryakaushik46@users.noreply.github.com>
2023-01-24 21:14:59 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app=FastAPI()
 origins=["*"] #list of urls that can talk to our api if * every thing can talk to our domain
 app.add_middleware(
     CORSMiddleware,
-    allow_origina=origins,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
